fd1635a19b9056d21efb564be926866c65342c37
JELaVallee <etienne.lavallee@betterment.com>
2022-12-08 21:02:03 -0500

    Setting up project for Day 07 challenge

---
+++ day_7_puzzle/filesystem_parser/bin/filesystem_parser.dart
@@ -0,0 +1,5 @@
+import 'package:filesystem_parser/filesystem_parser.dart' as filesystem_parser;
+
+void main(List<String> arguments) {
+  print('Hello world: ${filesystem_parser.calculate()}!');
+}
---
+++ day_7_puzzle/filesystem_parser/test/filesystem_parser_test.dart
@@ -0,0 +1,8 @@
+import 'package:filesystem_parser/filesystem_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
