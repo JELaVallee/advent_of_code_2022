cd8c5608fdcd5a2b749cfc9e595833956c756055
JELaVallee <etienne.lavallee@betterment.com>
2022-12-09 02:23:38 -0500

    Solution for both parts 1 and 2!

--- day_7_puzzle/filesystem_parser/test/filesystem_parser_test.dart
+++ day_7_puzzle/filesystem_parser/test/filesystem_parser_test.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
 import 'package:filesystem_parser/filesystem_parser.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('parses the filesystem tree', () {
+    final dataMapFile = File("./fs_cli_log_test.txt");
+    List<String> logLines = dataMapFile.readAsLinesSync();
+
+    final fsParser = FilesystemParser(logLines, 70000000);
+
+    expect(fsParser.directories.length, 4);
+    expect(fsParser.directories[0].totalFileSize, 48381165);
+    expect(fsParser.directories[1].totalFileSize, 94853);
+    expect(fsParser.directories[2].totalFileSize, 24933642);
+    expect(fsParser.directories[3].totalFileSize, 584);
+    expect(fsParser.totalFileSizeOfDirectoriesLessThan(100000), 95437);
+    expect(fsParser.directoryThatWillFree(30000000).totalFileSize, 24933642);
   });
 }
