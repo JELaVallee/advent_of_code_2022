c07a803c4b7c3fc262d5d20691f15c29eee1d586
JELaVallee <etienne.lavallee@betterment.com>
2022-12-09 19:43:21 -0500

    Setting up day 9 "Head-Tail Traversal" code

---
+++ day_9_puzzle/head_tail_follow/bin/head_tail_follow.dart
@@ -0,0 +1,5 @@
+import 'package:head_tail_follow/head_tail_follow.dart' as head_tail_follow;
+
+void main(List<String> arguments) {
+  print('Hello world: ${head_tail_follow.calculate()}!');
+}
---
+++ day_9_puzzle/head_tail_follow/test/head_tail_follow_test.dart
@@ -0,0 +1,8 @@
+import 'package:head_tail_follow/head_tail_follow.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
