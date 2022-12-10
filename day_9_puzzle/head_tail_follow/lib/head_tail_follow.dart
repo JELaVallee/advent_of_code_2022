b5367f76698f58a82de7e2e3e59786bd018ba388
JELaVallee <etienne.lavallee@betterment.com>
2022-12-10 17:09:11 -0500

    Solution for first task!

--- day_9_puzzle/head_tail_follow/lib/head_tail_follow.dart
+++ day_9_puzzle/head_tail_follow/lib/head_tail_follow.dart
@@ -1,3 +1,134 @@
-int calculate() {
-  return 6 * 7;
+import 'package:equatable/equatable.dart';
+
+class HeadTailFollow {
+  final List<String> commandList;
+  List<Command> commandStack = [];
+  List<Cell> headTraversals = [];
+  List<Cell> tailTraversals = [];
+
+  HeadTailFollow(this.commandList) {
+    parseCommands();
+    runTraversal();
+  }
+
+  int get totalTailVisitedCells {
+    return tailTraversals.toSet().length;
+  }
+
+  int get totalHeadVisitedCells {
+    return headTraversals.toSet().length;
+  }
+
+  void parseCommands() {
+    commandList.forEach((commandLine) {
+      final currentCommand = commandLine.split(" ");
+      commandStack.add(Command(Direction.values.byName(currentCommand[0]),
+          int.parse(currentCommand[1])));
+    });
+  }
+
+  void runTraversal() {
+    var currentHeadCell = Cell(0, 0);
+    var currentTailCell = currentHeadCell;
+
+    currentHeadCell.headVisited = true;
+    currentTailCell.tailVisited = true;
+
+    headTraversals.add(currentHeadCell);
+    tailTraversals.add(currentTailCell);
+
+    commandStack.forEach((command) {
+      print("Processing command: ${command.direction} ${command.distance}");
+      processCommand(command);
+    });
+  }
+
+  void processCommand(Command command) {
+    final int numberOfSteps = command.distance;
+    print("\tHead[start]: ${headTraversals.last}");
+    print("\tTail[start]: ${tailTraversals.last}");
+    for (var i = 0; i < numberOfSteps; i++) {
+      moveHead(command.direction);
+      if (tailNeedsToMove()) {
+        moveTailToHead();
+      }
+    }
+    print("\tHead[end]: ${headTraversals.last}");
+    print("\tTail[end]: ${tailTraversals.last}");
+  }
+
+  void moveHead(Direction direction) {
+    final Cell currentHeadCell = headTraversals.last;
+    final int newX = currentHeadCell.x + direction.offset.x;
+    final int newY = currentHeadCell.y + direction.offset.y;
+    final Cell nextCell = Cell(newX, newY);
+    nextCell.headVisited = true;
+    headTraversals.add(nextCell);
+  }
+
+  bool tailNeedsToMove() {
+    final currentHead = headTraversals.last;
+    final currentTail = tailTraversals.last;
+
+    final int distanceToHead = currentTail.distanceTo(currentHead);
+    final bool needsToMove = distanceToHead > 1;
+
+    return needsToMove;
+  }
+
+  void moveTailToHead() {
+    final currentHead = headTraversals.last;
+    final currentTail = tailTraversals.last;
+    Cell lastHead = headTraversals[headTraversals.length - 2];
+    lastHead.tailVisited = true;
+    tailTraversals.add(lastHead);
+  }
+}
+
+class Command {
+  final int distance;
+  final Direction direction;
+
+  Command(this.direction, this.distance);
+}
+
+class Coordinate2D {
+  final int x;
+  final int y;
+
+  const Coordinate2D(this.x, this.y);
+}
+
+enum Direction {
+  U(Coordinate2D(0, 1)),
+  D(Coordinate2D(0, -1)),
+  L(Coordinate2D(-1, 0)),
+  R(Coordinate2D(1, 0));
+
+  final Coordinate2D offset;
+
+  const Direction(this.offset);
+}
+
+class Cell extends Coordinate2D with EquatableMixin {
+  bool headVisited = false;
+  bool tailVisited = false;
+
+  Cell(x, y) : super(x, y);
+
+  int distanceTo(Cell toCell) {
+    final int distanceX = (toCell.x - x).abs();
+    final int distanceY = (toCell.y - y).abs();
+    final int distanceToCell = distanceX >= distanceY ? distanceX : distanceY;
+
+    return distanceToCell;
+  }
+
+  @override
+  String toString() {
+    return "($x, $y) head?: $headVisited, tail?: $tailVisited";
+  }
+
+  @override
+  List<Object?> get props => [x, y, headVisited, tailVisited];
 }
--- day_9_puzzle/head_tail_follow/test/head_tail_follow_test.dart
+++ day_9_puzzle/head_tail_follow/test/head_tail_follow_test.dart
@@ -1,8 +1,16 @@
+import 'dart:io';
+
 import 'package:head_tail_follow/head_tail_follow.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('parses the traversal command', () {
+    final dataMapFile = File("./head_commands_test.txt");
+    List<String> commandLines = dataMapFile.readAsLinesSync();
+
+    final fsParser = HeadTailFollow(commandLines);
+
+    expect(fsParser.totalHeadVisitedCells, 24);
+    expect(fsParser.totalTailVisitedCells, 13);
   });
 }
