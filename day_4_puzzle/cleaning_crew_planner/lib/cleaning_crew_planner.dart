39ebdebc42c4fcb7d93473413a9d21e877a49bd1
JELaVallee <etienne.lavallee@betterment.com>
2022-12-11 23:31:15 -0500

    Solution: Day 04: Task 1!

---
+++ day_4_puzzle/cleaning_crew_planner/lib/cleaning_crew_planner.dart
@@ -0,0 +1,71 @@
+class CleaningCrewPlanner {
+  List<String> crewAssignments;
+  List<CrewPair> crewPairs = [];
+
+  CleaningCrewPlanner(this.crewAssignments) {
+    parseAssignments();
+  }
+
+  int get numberOfFullyOverlappedAssignments {
+    return crewPairs.where((pair) => pair.inclusive).length;
+  }
+
+  void parseAssignments() {
+    crewAssignments.forEach((assignmentListing) {
+      crewPairs.add(CrewPair(assignmentListing));
+    });
+  }
+}
+
+class CrewPair {
+  final String assignmentListing;
+  final SectionRange crew1Section = SectionRange(0, 0);
+  final SectionRange crew2Section = SectionRange(0, 0);
+  bool _isOverlapping = false;
+  bool _isInclusive = false;
+
+  CrewPair(this.assignmentListing) {
+    parseAssignmentListing();
+  }
+
+  bool get overlapping {
+    return _isOverlapping;
+  }
+
+  bool get inclusive {
+    return _isInclusive;
+  }
+
+  void parseAssignmentListing() {
+    final crewRangeIds = assignmentListing.split(",");
+    final crewRange1Start = int.parse(crewRangeIds[0].split("-")[0]);
+    final crewRange1End = int.parse(crewRangeIds[0].split("-")[1]);
+    final crewRange2Start = int.parse(crewRangeIds[1].split("-")[0]);
+    final crewRange2End = int.parse(crewRangeIds[1].split("-")[1]);
+    crew1Section.start = crewRange1Start;
+    crew1Section.end = crewRange1End;
+    crew2Section.start = crewRange2Start;
+    crew2Section.end = crewRange2End;
+    updateRangeFlags();
+  }
+
+  void updateRangeFlags() {
+    _isInclusive |= crew1Section.start <= crew2Section.start &&
+        crew1Section.end >= crew2Section.end;
+    _isInclusive |= crew2Section.start <= crew1Section.start &&
+        crew2Section.end >= crew1Section.end;
+    _isOverlapping |= _isInclusive ||
+        (crew1Section.start <= crew2Section.end &&
+            crew1Section.end >= crew2Section.start);
+    _isOverlapping |= _isInclusive ||
+        (crew2Section.start <= crew1Section.end &&
+            crew2Section.end >= crew1Section.start);
+  }
+}
+
+class SectionRange {
+  int start = 0;
+  int end = 0;
+
+  SectionRange(this.start, this.end);
+}
---
+++ day_4_puzzle/cleaning_crew_planner/test/cleaning_crew_planner_test.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:test/test.dart';
+import 'package:cleaning_crew_planner/cleaning_crew_planner.dart';
+
+void main() {
+  test(
+      'processes the cleaning assignments and get the number of fully-overlapping assignments [test]',
+      () {
+    final dataMapFile = File("./cleaning_crew_assignments_test.txt");
+    List<String> crewAssignments = dataMapFile.readAsLinesSync();
+
+    CleaningCrewPlanner cleaningCrewPlanner =
+        CleaningCrewPlanner(crewAssignments);
+
+    expect(cleaningCrewPlanner.numberOfFullyOverlappedAssignments, 2);
+  });
+  test(
+      'processes the cleaning assignments and get the number of fully-overlapping assignments [full]',
+      () {
+    final dataMapFile = File("./cleaning_crew_assignments_full.txt");
+    List<String> crewAssignments = dataMapFile.readAsLinesSync();
+
+    CleaningCrewPlanner cleaningCrewPlanner =
+        CleaningCrewPlanner(crewAssignments);
+
+    expect(cleaningCrewPlanner.numberOfFullyOverlappedAssignments, 588);
+  });
+}
