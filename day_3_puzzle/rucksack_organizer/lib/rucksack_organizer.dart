49e2612b768314d6c48957f8a35e5bb1db5a9796
JELaVallee <etienne.lavallee@betterment.com>
2022-12-11 22:07:29 -0500

    Solution: Day 03: Task 2...three more days!

--- day_3_puzzle/rucksack_organizer/lib/rucksack_organizer.dart
+++ day_3_puzzle/rucksack_organizer/lib/rucksack_organizer.dart
@@ -1,10 +1,9 @@
-import 'dart:math';
-
 import 'package:equatable/equatable.dart';
 
 class RucksackOrganizer {
   final List<String> rucksackInventory;
   final List<Rucksack> rucksacks = [];
+  final List<ElfGroup> elfGroups = [];
 
   RucksackOrganizer(this.rucksackInventory) {
     parseInventory();
@@ -17,10 +16,50 @@ class RucksackOrganizer {
         .reduce((value, priority) => value + priority);
   }
 
+  int get totalPriorityOfBadgeItems {
+    return elfGroups
+        .map((elfGroup) => elfGroup.badgeItem.priority)
+        .reduce((value, priority) => value + priority);
+  }
+
   void parseInventory() {
+    List<Rucksack> elfGroupSacks = [];
     rucksackInventory.forEach((inventoryList) {
-      rucksacks.add(Rucksack(inventoryList));
+      final rucksack = Rucksack(inventoryList);
+      rucksacks.add(rucksack);
+      elfGroupSacks.add(rucksack);
+      if (elfGroupSacks.length == 3) {
+        elfGroups.add(ElfGroup(elfGroupSacks));
+        elfGroupSacks = [];
+      }
     });
+  }
+}
+
+class ElfGroup {
+  final List<Rucksack> groupRucksacks;
+  ElfGroup(this.groupRucksacks);
+  Item get badgeItem {
+    final inventory1 = <Item>{
+      ...groupRucksacks[0].compartment1,
+      ...groupRucksacks[0].compartment2
+    }.toSet().toList();
+
+    final inventory2 = <Item>{
+      ...groupRucksacks[1].compartment1,
+      ...groupRucksacks[1].compartment2
+    }.toSet().toList();
+
+    final inventory3 = <Item>{
+      ...groupRucksacks[2].compartment1,
+      ...groupRucksacks[2].compartment2
+    }.toSet().toList();
+
+    final Item badgeItem = inventory1
+        .toSet()
+        .intersection(inventory2.toSet().intersection(inventory3.toSet()))
+        .first;
+    return badgeItem;
   }
 }
 
--- day_3_puzzle/rucksack_organizer/test/rucksack_organizer_test.dart
+++ day_3_puzzle/rucksack_organizer/test/rucksack_organizer_test.dart
@@ -23,4 +23,24 @@ void main() {
 
     expect(rucksackOrganizer.totalPriorityOfSharedItems, 8176);
   });
+  test(
+      'processes the rucksack inventory and sum the priorities of the badge items [test]',
+      () {
+    final dataMapFile = File("./rucksack_inventory_test.txt");
+    List<String> rucksackInventory = dataMapFile.readAsLinesSync();
+
+    RucksackOrganizer rucksackOrganizer = RucksackOrganizer(rucksackInventory);
+
+    expect(rucksackOrganizer.totalPriorityOfBadgeItems, 70);
+  });
+  test(
+      'processes the rucksack inventory and sum the priorities of the badge items [full]',
+      () {
+    final dataMapFile = File("./rucksack_inventory_full.txt");
+    List<String> rucksackInventory = dataMapFile.readAsLinesSync();
+
+    RucksackOrganizer rucksackOrganizer = RucksackOrganizer(rucksackInventory);
+
+    expect(rucksackOrganizer.totalPriorityOfBadgeItems, 2689);
+  });
 }
